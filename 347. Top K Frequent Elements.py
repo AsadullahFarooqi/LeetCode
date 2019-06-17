adbbb71b9b9aeeca10a33b50a1586ee8aafc844a
Asadullah Farooqi <asadullah.itcgcs@gmail.com>
2019-06-17 16:57:36 +0500

    documentation for 347 added

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -3,30 +3,43 @@
 Given a non-empty array of integers, return the k most frequent elements.
 
 Example 1:
+    Input: nums = [1,1,1,2,2,3], k = 2
+    Output: [1,2]
 
-Input: nums = [1,1,1,2,2,3], k = 2
-Output: [1,2]
 Example 2:
+    Input: nums = [1], k = 1
+    Output: [1]
 
-Input: nums = [1], k = 1
-Output: [1]
 Note:
-
-You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
-Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+    You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+    Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
 """
 
 
 def topKFrequent(nums, k):
-
+    """The algorithm works in the following steps
+    1 - It makes an hash table to store the # of appearence 
+    2 - Sorting the hash table keys by their values in reverse order
+    3 - Returning the first k values
+    
+    Args:
+        nums (TYPE): Description
+        k (TYPE): Description
+    
+    Returns:
+        TYPE: Description
+    """
+    # step 1
     count_hash = {}
     for i in nums:
         if i in count_hash:
             count_hash[i] += 1
             continue
         count_hash[i] = 1
+    # step 2
     count_hash = sorted(count_hash, reverse=True, key=lambda item: count_hash[item])
 
+    # steop 3
     return count_hash[:k]
 
 if __name__ == '__main__':
