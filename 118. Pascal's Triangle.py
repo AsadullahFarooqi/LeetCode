40f04881bc64d376005c11b6c6fd8d2907a728fb
Asadullah Farooqi <asadullah.itcgcs@gmail.com>
2019-11-12 10:22:29 +0500

    modified story.txt, bullets added fro front page

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -2,20 +2,25 @@
 [
 [1],
 [1,1],
-[1,2,1],
+ [1,2,1],
 [1,3,3,1],
 [1,4,6,4,1]
 ]
 """
-def solution(numrows):
+class Solution:
+    def generate(self, numRows):
+        ans = [[1]]
+        row = 1
+        prev_r_cols = 0
+        while row <= numRows:
+            r = [1]
+            for c in range(1, prev_r_cols):
+                r.append(ans[row-1][c-1] + ans[row-1][c])
+            r.append(1)
+            prev_r_cols += 1
+            row += 1
+        print(ans)
 
-    for row in range(0, numrows+1):
-        temp = []
-        for cols in range(row):
-            temp.append(row)
-        print(temp)
 if __name__ == '__main__':
     k = 5
-    solution(k)
-    # for i in solution(k):
-    #     print(i)
+    sol = Solution().generate(k)
